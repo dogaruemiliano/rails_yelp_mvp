3406c7fbbec06eae0417a04e2d1560552eedfc69
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-11 22:59:01 +0100

    all done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,22 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(params_restaurants)
+
+    if @restaurant.save
+      redirect_to(restaurant_path(@restaurant.id))
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_restaurants
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
 
 #
